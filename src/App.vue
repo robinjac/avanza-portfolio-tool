23f80dc7a2124505cde6e286aeb29eb27f8f5179
robinjac <robin23jac@gmail.com>
2023-02-12 20:38:59 +0100

    Remove imports of junk

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <p>Hello World</p>
 </template>
