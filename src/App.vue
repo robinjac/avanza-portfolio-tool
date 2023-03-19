0f80d47072826b24518f89c53b63bf1a04d22b87
robinjac <robin23jac@gmail.com>
2023-03-19 11:37:27 +0100

    Type json structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <script lang="ts">
-type ItemList = Array<{ id: string; name: string }>;
-import funds from "../assets/funds.json";
+import fundsData from "../assets/funds.json";
 
 export default {
   data() {
     return {
-      items: [] as ItemList,
+      funds: fundsData as Array<Fund>,
     };
   },
   mounted() {
     // Fetch data from API
-    console.log(funds);
   },
 };
 </script>
 
 <template>
   <div>
-    <h2>List of items:</h2>
-    <ul>
-      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
-    </ul>
+    <h2>List of funds:</h2>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="fund in funds" :key="fund.isin">
+          <td>{{ fund.name }}</td>
+        </tr>
+      </tbody>
+    </v-table>
   </div>
 </template>
