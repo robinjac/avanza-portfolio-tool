de43592c982fd8bec971d71d4fc32ab0cdcd78d8
robinjac <robin23jac@gmail.com>
2023-03-24 15:40:08 +0100

    Adjust layout with grids

--- src/App.vue
+++ src/App.vue
@@ -33,32 +33,50 @@ export default {
 
 <template>
   <v-container>
-    <v-select
-      clearable
-      label="Selected columns"
-      :items="availableColumns"
-      :model-value="selectedColumns"
-      @update:model-value="handleSelect"
-      multiple
-      variant="solo"
-    ></v-select>
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">name</th>
-          <th v-for="column in selectedColumns" class="text-left">
-            {{ column }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="fund in funds" :key="fund.isin">
-          <td>{{ fund.name }}</td>
-          <td v-for="column in selectedColumns">
-            {{ (fund as unknown as NumericValues)[column] }}
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+    <v-row>
+      <v-col>
+        <v-select
+          label="Selected columns"
+          :items="availableColumns"
+          :model-value="selectedColumns"
+          @update:model-value="handleSelect"
+          multiple
+          variant="solo"
+          hide-details
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-row align="end">
+      <v-sheet class="pa-2 ma-2">
+        <v-btn class="ml-6" size="small" variant="outlined"> Clear </v-btn>
+        <v-btn class="mx-2" size="small" variant="text"> Reset </v-btn>
+      </v-sheet>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">name</th>
+                <th v-for="column in selectedColumns" class="text-left">
+                  {{ column }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="fund in funds" :key="fund.isin">
+                <td>{{ fund.name }}</td>
+                <td v-for="column in selectedColumns">
+                  {{ (fund as unknown as NumericValues)[column] }}
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
