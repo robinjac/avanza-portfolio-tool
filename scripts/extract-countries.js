25778d52fa4e1b960780ecdd0f8dc6e2c5c412a0
robin jacobsson <robin.jacobsson@kebne.se>
2023-04-25 23:27:16 +0200

    Extract countries

---
+++ scripts/extract-countries.js
@@ -0,0 +1,20 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import path from "node:path";
+
+const baseURL = process.cwd();
+
+const portfolios = JSON.parse(readFileSync(path.resolve(baseURL, "./assets/portfolio-data.json"), "utf-8"));
+const toFunds = {};
+
+for (const { countryChartData, id } of portfolios) {
+    for (const { name } of countryChartData) {
+        if (toFunds[name] === undefined) {
+            toFunds[name] = [id];
+        } else {
+            toFunds[name].push(id);
+        }
+    }
+}
+
+writeFileSync(path.resolve(baseURL, "./assets/countries.json"), JSON.stringify(Object.keys(toFunds), null, 4));
+writeFileSync(path.resolve(baseURL, "./assets/countries-funds.json"), JSON.stringify(toFunds, null, 4));
