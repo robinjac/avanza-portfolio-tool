5220bf6f695c9ca2d9086e05c8185ba748addbab
robin jacobsson <robin.jacobsson@kebne.se>
2023-04-25 23:02:50 +0200

    Improve extract categories

--- scripts/extract-categories.js
+++ scripts/extract-categories.js
@@ -2,31 +2,25 @@ import { readFileSync, writeFileSync } from "node:fs";
 import path from "node:path";
 
 const baseURL = process.cwd();
+const trim = (cat) => cat.trim();
+const funds = JSON.parse(readFileSync(path.resolve(baseURL, "./assets/funds-transformed.json"), "utf-8"));
+const toCategories = {};
+const toFunds = {};
 
-const funds = JSON.parse(readFileSync(path.resolve(baseURL, "./assets/funds.json"), "utf-8"));
-const fundToCategories = {};
-const categoryToFunds = {};
-
-for (const fund of funds) {
-    const categories = fund.category.split(",").map((cat) => cat.trim());
+for (const { Id, Category } of funds) {
+    const categories = Category.split(",").map(trim);
 
     for (const category of categories) {
-        categoryToFunds[category] = [];
-    }
-
-    fundToCategories[fund.orderbookId] = categories;
-}
-
-const categoryLabels = Object.keys(categoryToFunds);
-
-for (const category of categoryLabels) {
-    for (const fund of funds) {
-        if (fundToCategories[fund.orderbookId].includes(category)) {
-            categoryToFunds[category].push(fund.orderbookId);
+        if (toFunds[category] === undefined) {
+            toFunds[category] = [Id];
+        } else {
+            toFunds[category].push(Id);
         }
     }
+
+    toCategories[Id] = categories;
 }
 
-writeFileSync(path.resolve(baseURL, "./assets/categories.json"), JSON.stringify(categoryLabels, null, 4));
-writeFileSync(path.resolve(baseURL, "./assets/categories-funds.json"), JSON.stringify(categoryToFunds, null, 4));
-writeFileSync(path.resolve(baseURL, "./assets/funds-categories.json"), JSON.stringify(fundToCategories, null, 4));
+writeFileSync(path.resolve(baseURL, "./assets/categories.json"), JSON.stringify(Object.keys(toFunds), null, 4));
+writeFileSync(path.resolve(baseURL, "./assets/categories-funds.json"), JSON.stringify(toFunds, null, 4));
+writeFileSync(path.resolve(baseURL, "./assets/funds-categories.json"), JSON.stringify(toCategories, null, 4));
